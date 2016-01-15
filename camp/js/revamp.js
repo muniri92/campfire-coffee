4c77a0f6622518af2228173b3561c7978859e7cc
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-01-15 15:12:19 -0800

    final js code finished, gotta work on css still

--- camp/js/revamp.js
+++ camp/js/revamp.js
@@ -1,8 +1,7 @@
-
+'use strict';
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
-
 var places = [];
-// Object Constructor
+// OBJECT CONSTRUCTOR
 function Camp(kiosk, minCust, maxCust, avgCupPerCust, avgLbsPerCust) {
   this.kiosk = kiosk;
   this.minCust = minCust;
@@ -16,36 +15,34 @@ function Camp(kiosk, minCust, maxCust, avgCupPerCust, avgLbsPerCust) {
   this.custDaily = 0;
   this.cupsDaily = 0;
   this.lbsDaily = 0;
+  this.generateNumbers();
+  this.generateDaily();
   places.push(this);
-
 }
-
+// GENERATED DATA METHOD IN THE CAMP PROTOTYPE
 Camp.prototype.generateNumbers = function() {
   for (var j = 0; j < hours.length; j++) {
     this.randomCust[j] = Math.floor((Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
     parseInt(this.randomCust[j]);
     this.cupsHourly[j] = +(this.randomCust[j] * this.avgCupPerCust).toPrecision(3);
-    //parseFloat(this.totalCupsHr[j]);
     this.cupsToPounds[j] = +(this.cupsHourly[j] / 20).toFixed(2);
     this.lbsHourly[j] = +(this.randomCust[j] * this.avgLbsPerCust).toFixed(2);
-    //parseFloat(this.totalCoffee[j]);
   }
 }
-
-// Generate All Daily Totals
+// ALL DAILY TOTALS METHOD IN THE CAMP PROTOTYPE
 Camp.prototype.generateDaily = function () {
   for (var m = 0; m < hours.length; m++) {
     this.custDaily += this.randomCust[m];
     this.cupsDaily += this.cupsHourly[m];
-    this.lbsDaily += (this.lbsHourly[m] + this.cupsToPounds[m]);
+    this.lbsDaily += Math.ceil(parseInt(this.lbsHourly[m]) + parseInt(this.cupsToPounds[m]));
   }
 };
-
-// Table to Print Data Information
+// CREATE A TABLE
 var section = document.getElementById('table1');
 var table = document.createElement('table')
-table.className = 'tab1';
-function table1()  {
+function table1() {
+  // FILLS TOP ROW
+  table.className = 'tab1';
   var topRow = document.createElement('tr');
   topRow.className = 'th1';
   var locations = document.createElement('th');
@@ -62,14 +59,13 @@ function table1()  {
   headerTotal.className = 'th1';
   topRow.appendChild(headerTotal);
   table.appendChild(topRow);
-
+  // FILL ROWS WITH DATA
   for (var j = 0; j < places.length; j++) {
     var row = document.createElement('tr');
     var kioskNames = document.createElement('th');
     kioskNames.textContent = places[j].kiosk;
     kioskNames.className = 'th1';
     row.appendChild(kioskNames);
-    // fix
     for (var k = 0; k < hours.length; k++) {
       var dataRow = document.createElement('td');
       dataRow.textContent = places[j].lbsHourly[k];
@@ -78,96 +74,65 @@ function table1()  {
     var rowTotal = document.createElement('th');
     rowTotal.textContent = places[j].lbsDaily.toFixed(2);
     rowTotal.className = 'total';
-
     row.appendChild(rowTotal);
     table.appendChild(row);
   }
   section.appendChild(table);
-}
-
-// FORM
-
+};
+// CREATE A NEW ROW
+function renderNewRow(newForm) {
+  var row = document.createElement('tr');
+  var headPlace = document.createElement('th');
+  headPlace.textContent = places[places.length-1].kiosk;
+  headPlace.className = 'th1';
+  row.appendChild(headPlace);
+  // Fills in the cell of each time
+  for (var j = 0; j < hours.length; j++) {
+    var cell = document.createElement('td');
+    cell.textContent = places[places.length-1].lbsHourly[j];
+    row.appendChild(cell);
+  }
+  // Creates the Total
+  var lastTotal = document.createElement('td');
+  lastTotal.textContent = +places[places.length-1].lbsDaily.toFixed(2);
+  lastTotal.className = 'total';
+  row.appendChild(lastTotal);
+  table.appendChild(row);
+};
+// EVENT HANDLER
 var inputData = document.getElementById('inputData');
-var clearTable = document.getElementById('clear-table');
 var newRow = document.getElementById('newRow')
-
 function handleForm(event) {
   event.preventDefault();
-
   if ( !event.target.loc.value || !event.target.min.value || !event.target.max.value || !event.target.cup.value || !event.target.lbs.value ) {
     return alert('Fields cannot be empty!');
   }
-
   var newLoc = event.target.loc.value;
   var newMin = event.target.min.value;
   var newMax = event.target.max.value;
   var newCup = event.target.cup.value;
   var newLbs = event.target.lbs.value;
-
   var newForm = new Camp(newLoc, newMin, newMax, newCup, newLbs);
   newForm.generateNumbers();
   newForm.generateDaily();
-
-  console.log('New data was present with location name: ' + event.target.loc.value + ', minimum customers: ' + event.target.min.value + ', maximum customers: ' + event.target.max.value + ', average cups (per hr): ' + event.target.cup.value + ', and average lbs (per hr): ' + event.target.lbs.value + ' at ' + Date());
-
-  //places.push.(newForm);
-  function renderNewLine() {
-      // Creates the Location Names
-      var row = document.createElement('tr');
-      var headPlace = document.createElement('th');
-      headPlace.textContent = newLoc;
-      headPlace.className = 'th1';
-      row.appendChild(headPlace);
-      // Fills in the cell of each time
-      for (var j = 0; j < hours.length; j++) {
-        var cell = document.createElement('td');
-        cell.textContent = places[places.length-1].lbsHourly[j];
-        row.appendChild(cell);
-      }
-      // Creates the Total
-      var lastTotal = document.createElement('td');
-      lastTotal.textContent = +places[places.length-1].lbsDaily.toFixed(2);
-      lastTotal.className = 'total';
-      row.appendChild(lastTotal);
-      table.appendChild(row);
-    };
-    renderNewLine();
-    event.target.loc.value = null;
-    event.target.min.value = null;
-    event.target.max.value = null;
-    event.target.cup.value = null;
-    event.target.lbs.value = null;
+  //console.log('New data was present with location name: ' + event.target.loc.value + ', minimum customers: ' + event.target.min.value + ', maximum customers: ' + event.target.max.value + ', average cups (per hr): ' + event.target.cup.value + ', and average lbs (per hr): ' + event.target.lbs.value + ' at ' + Date());
+  // PUSH NEW DATA TO PLACES ARRAY
+  places.push(newForm);
+  renderNewRow(newForm);
+  // ERASES VALUES IN 'newForm'
+  event.target.loc.value = null;
+  event.target.min.value = null;
+  event.target.max.value = null;
+  event.target.cup.value = null;
+  event.target.lbs.value = null;
 }
-
+// RUN EVENT LISTENER
 newRow.addEventListener('submit', handleForm);
-// clearTable.addEventListener('click', function() {
-//   console.log('You just cleared all the newlt generated tables');
-//   // inputData.innerHTML = '';
-//   document.getElementById("myTable").deleteRow(0)
-//
-//
-//   // newTableData = [];
-// });
-
-// Call
+// CALL FUNCTION
 var pike = new Camp('Pike Place Market', 14, 55, 1.2, 3.7);
-pike.generateNumbers();
-pike.generateDaily();
 var cap = new Camp('Capital Hill', 32, 48, 3.2, 0.4);
-cap.generateNumbers();
-cap.generateDaily();
 var sea = new Camp('Seattle Public Library', 49, 75, 2.6, 0.2);
-sea.generateNumbers();
-sea.generateDaily();
 var slu = new Camp('South Lake Union', 35, 88, 1.3, 3.7);
-slu.generateNumbers();
-slu.generateDaily();
 var seatac = new Camp('Sea-Tac Airport', 68, 124, 1.1, 2.7);
-seatac.generateNumbers();
-seatac.generateDaily();
 var web = new Camp('Website Sales', 3, 6, 0, 6.7);
-web.generateNumbers();
-web.generateDaily();
-
-// Table
 table1();
